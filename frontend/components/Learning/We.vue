3a8ac9d9f6564e264b3da2843146dcdc6a5cbf1b
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-01-29 01:49:29 +0500

    Add animations for icons

--- frontend/components/Learning/We.vue
+++ frontend/components/Learning/We.vue
@@ -1,5 +1,8 @@
 <template>
-<div class="learning__we learning-we" v-if="data.list.length">
+<div
+    class="learning__we learning-we"
+    v-if="data.list"
+>
   <div class="learning__container container">
     <div class="learning-we__block">
       <GlobalBlockHeader :title="data.title" class-title="fz-h1--tablet" />
@@ -10,10 +13,20 @@
             <div class="learning-we__item-text fz-h3 fz-h2--mobile" v-html="item.text"></div>
           </div>
         </div>
-        <div class="learning-we__media" v-if="data.image">
-          <picture class="learning-we__media-pic">
-            <img :src="data.image" :alt="data.title">
-          </picture>
+        <div class="learning-we__media">
+          <svg width="520" height="520" viewBox="0 0 520 520" fill="none" xmlns="http://www.w3.org/2000/svg" ref="animatedSvg">
+            <circle cx="260" cy="260" r="259.25" stroke="white" stroke-width="1.5"/>
+            <path d="M260.007 507C281.421 507 298.781 396.414 298.781 260C298.781 123.586 281.421 13 260.007 13C238.592 13 221.232 123.586 221.232 260C221.232 396.414 238.592 507 260.007 507Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260 298.774C396.414 298.774 507 281.414 507 259.999C507 238.585 396.414 221.224 260 221.224C123.586 221.224 13 238.585 13 259.999C13 281.414 123.586 298.774 260 298.774Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260 299.042C396.414 299.042 507 281.682 507 260.267C507 238.852 396.414 221.492 260 221.492C123.586 221.492 13 238.852 13 260.267C13 281.682 123.586 299.042 260 299.042Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260.007 507C281.421 507 298.781 396.414 298.781 260C298.781 123.586 281.421 13 260.007 13C238.592 13 221.232 123.586 221.232 260C221.232 396.414 238.592 507 260.007 507Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260 298.774C396.414 298.774 507 281.414 507 259.999C507 238.585 396.414 221.224 260 221.224C123.586 221.224 13 238.585 13 259.999C13 281.414 123.586 298.774 260 298.774Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M434.666 434.624C449.808 419.482 383.888 329.011 287.429 232.551C190.969 136.092 100.498 70.1712 85.3553 85.3136C70.2128 100.456 136.133 190.927 232.593 287.387C329.052 383.846 419.524 449.767 434.666 434.624Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260.007 507C281.421 507 298.781 396.414 298.781 260C298.781 123.586 281.421 13 260.007 13C238.592 13 221.232 123.586 221.232 260C221.232 396.414 238.592 507 260.007 507Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M260 298.774C396.414 298.774 507 281.414 507 259.999C507 238.585 396.414 221.224 260 221.224C123.586 221.224 13 238.585 13 259.999C13 281.414 123.586 298.774 260 298.774Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M287.429 287.392C383.888 190.933 449.808 100.461 434.666 85.3189C419.524 70.1765 329.052 136.097 232.593 232.556C136.133 329.016 70.2128 419.487 85.3553 434.63C100.498 449.772 190.969 383.852 287.429 287.392Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            <path d="M434.666 434.624C449.808 419.482 383.888 329.011 287.429 232.551C190.969 136.092 100.498 70.1712 85.3553 85.3136C70.2128 100.456 136.133 190.927 232.593 287.387C329.052 383.846 419.524 449.767 434.666 434.624Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+          </svg>
         </div>
       </div>
     </div>
@@ -22,6 +35,9 @@
 </template>
 
 <script setup lang="ts">
+import Vivus from "vivus";
+import { vElementVisibility } from '@vueuse/components';
+
 defineProps<{
   data: {
     image?: string,
@@ -30,6 +46,15 @@ defineProps<{
   }
 }>();
 
+const animatedSvg = ref(null);
+const vivus = ref( {} );
+
+onMounted( () => {
+  vivus.value = new Vivus(animatedSvg.value, {
+    duration: 200,
+    animTimingFunction: Vivus.EASE
+  });
+} )
 </script>
 
 <style scoped lang="scss">
