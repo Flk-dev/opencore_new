4b7e248778f70298dd0f547d223fc956652c5773
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-02-09 13:43:15 +0500

    Fix page transition

--- frontend/components/Global/Transition.vue
+++ frontend/components/Global/Transition.vue
@@ -4,6 +4,14 @@
       <div class="transition__first-inner">
         <div class="transition__bg"></div>
         <div class="transition__text">
+          <div class="transition__text-logo">
+            <svg width="595" height="163" viewBox="0 0 595 163" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M23.9233 139.363C7.97442 123.699 0 104.427 0 81.4531C0 58.5741 7.97442 39.3025 23.9233 23.5435C39.8721 7.8795 59.6183 0 83.1619 0C106.705 0 126.452 7.8795 142.4 23.5435C158.349 39.2076 166.324 58.4791 166.324 81.4531C166.324 104.332 158.349 123.604 142.4 139.363C126.452 155.027 106.705 162.906 83.1619 162.906C59.6183 162.811 39.8721 155.027 23.9233 139.363ZM47.7516 44.6188C38.1633 54.5868 33.4166 66.8333 33.4166 81.4531C33.4166 96.0729 38.1633 108.319 47.7516 118.287C57.3399 128.255 69.1117 133.192 83.1619 133.192C97.307 133.192 109.174 128.255 118.667 118.287C128.16 108.319 132.907 96.0729 132.907 81.4531C132.907 66.8333 128.16 54.5868 118.667 44.6188C109.174 34.6508 97.307 29.7142 83.1619 29.7142C69.1117 29.7142 57.3399 34.6508 47.7516 44.6188Z" fill="white"/>
+              <path d="M218.551 160.535H186.273V2.375H253.202C270.48 2.375 284.53 7.31155 295.257 17.2796C305.985 27.2476 311.301 39.8738 311.301 55.2531C311.301 70.6323 305.985 83.1636 295.352 92.8468C284.72 102.53 270.669 107.467 253.202 107.467H218.646V160.535H218.551ZM218.551 31.045V78.7017H253.107C260.512 78.7017 266.397 76.5182 270.954 72.0563C275.511 67.5945 277.695 61.8984 277.695 54.9683C277.695 48.0381 275.416 42.3421 270.954 37.7853C266.397 33.2285 260.512 31.045 253.107 31.045H218.551Z" fill="white"/>
+              <path d="M431.486 160.535H326.109V2.375H431.486V31.045H358.482V67.2147H429.207V95.6948H358.482V131.865H431.486V160.535Z" fill="white"/>
+              <path d="M562.298 153.604V2.375H594.575V160.629H531.729L485.496 9.30516V160.535H453.219V2.375H515.875L562.298 153.604Z" fill="white"/>
+            </svg>
+          </div>
           <div class="transition__text-inner">
             <div class="transition__text-item transition__text--first" data-word-active="true">core</div>
             <div class="transition__text-item transition__text--secondary" data-word-active="false">steady</div>
@@ -21,20 +29,7 @@
 </template>
 
 <script setup lang="ts">
-//import {initWords, initPageLoader} from "~/utils/pageTransition";
-
 usePagePreloaderLogic();
-
-// const nuxtApp = useNuxtApp();
-// nuxtApp.hook("page:start", () => {
-//   console.log( 'Page start' );
-//   initWords();
-// });
-//
-// nuxtApp.hook("page:finish", () => {
-//   console.log( 'Page finish' );
-//   initPageLoader();
-// });
 </script>
 
 <style scoped lang="scss">
@@ -95,14 +90,34 @@ usePagePreloaderLogic();
     position: absolute;
     width: 100%;
     bottom: 4.2rem;
-    padding: 0 3.3rem;
+    padding: 0 0 0 2.9rem;
 
+    display: flex;
+    align-items: center;
+
+    &-logo {
+      min-width: 59.4rem;
+      margin-right: 2rem;
+      margin-top: 4rem;
+
+      & svg {
+        width: 100%;
+        height: auto;
+      }
+    }
+
+    //&-logo {
+    //  position: absolute;
+    //  left: 0;
+    //  bottom: 0;
+    //}
+    //
     &-item {
       font-family: var(--ff-atyp);
       font-style: italic;
       font-size: 23.3rem;
       line-height: 80%;
-      letter-spacing: -.4rem;
+      letter-spacing: -2.2rem;
       text-transform: uppercase;
       color: var(--fg-white);
       display: none;
