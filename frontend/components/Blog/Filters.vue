ae517e0b327810cf698294a4d536d73b2d8a8cec
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-02-16 23:31:31 +0500

    Add blog filters

--- frontend/components/Blog/Filters.vue
+++ frontend/components/Blog/Filters.vue
@@ -1,15 +1,21 @@
 <template>
-  <GlobalFilterCategories class="blog__filter" :data="categories" />
+  <GlobalFilterCategories class="blog__filter" :style="{top: (!isHide ? (headerHeight - 2) : 0) + 'px'}" :data="categories" />
 </template>
 
 <script setup lang="ts">
+import { useHeader } from "~/stores/header";
+
 const { result: categories } = await useApi( '/blog/categories', {}, 'blog/categories' );
+
+const store = useHeader();
+const isHide = computed(() => store.isHide);
+const headerHeight = computed(() => store.headerHeight);
 </script>
 
 <style scoped lang="scss">
 .blog__filter {
   position: sticky;
-  top: 4.5rem;
+  top: 0;
   z-index: 50;
   background: var(--fg-white);
   padding: 1rem 0;
