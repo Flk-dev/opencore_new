cd22b1e2bfcf2a64d6b0976950b62adf5f4a47d6
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-03-07 02:35:31 +0500

    Fix route methodology

--- frontend/components/Methodology/Route.vue
+++ frontend/components/Methodology/Route.vue
@@ -3,40 +3,63 @@
     <div class="container">
       <GlobalBlockHeader :title="data.title" classes="route" class-title="fz-h1" />
       <div class="route__grid">
-        <div class="route-item" v-for="(item, dataKey) in data.select" :key="dataKey">
-          <div class="route-item__line"></div>
-          <div class="route-item__left fz-h3">
-            <div class="route-item__sticky">
-              <div class="route-item__counter fz-h1--mobile">{{ dataKey + 1 }}</div>
-              <h3 class="route-item__title fz-h1--mobile" v-if="item.post_title" v-html="fixText(item.post_title)"></h3>
-              <GlobalRoundVideo class="route-item__video" v-if="item.video_preview && item.video_link" :preview="item.video_preview" :link="item.video_link"  />
+        <div class="route__col route__left">
+          <div class="route__line route__line--first"></div>
+          <div
+              class="route__titles"
+              :style="{top: (!isHide ? (headerHeight + 10) : 20) + 'px'}"
+          >
+            <div
+                class="route__titles-item"
+                v-for="(item, dataKey) in data.select"
+                :key="dataKey"
+                :data-id="dataKey"
+                :class="{ 'route__titles-item--active': dataKey === 0 }"
+                ref="titles"
+            >
+              <div class="route__titles-counter fz-h3 fz-h1--mobile">{{ dataKey + 1 }}</div>
+              <h3 class="route__titles-title fz-h1--mobile" v-if="item.post_title" v-html="fixText(item.post_title)"></h3>
             </div>
           </div>
-          <div class="route-item__right">
-            <div class="route-item__text">
-              <ContentText v-if="item.text" :text="fixText(item.text)" />
-              <GlobalRoundVideo class="route-item__video _tablet" v-if="item.video_preview && item.video_link" :preview="item.video_preview" :link="item.video_link"  />
-            </div>
-            <div class="route-item__accordion accordion" v-if="item.list.length">
-              <AccordionItem
-                  v-for="(list, key) in item.list"
-                  :key="key"
-                  :title="list.title"
-                  :counter="( dataKey + 1 ) + '.' + ( key + 1 )"
-                  :text="list.list"
-                  :is-two-column="list.is_two_colums"
-                  title-class="fz-h4"
-                  :is-white="true"
-                  class="_small route-item__accordion-item"
-              />
-            </div>
-            <div class="route-item__result" v-if="item.result">
-              <div class="route-item__subtitle fz-h4 fz-h1--mobile">Результат</div>
+          <div class="route__line route__line--last"></div>
+        </div>
+        <div class="route__col route__right">
+          <div class="route__line route__line--first"></div>
+          <div class="route__line route__line--vertical"></div>
+          <div class="route__row">
+            <div
+                class="route-item"
+                v-for="(item, dataKey) in data.select"
+                :key="dataKey"
+                :data-id="dataKey"
+                ref="items"
+            >
               <div class="route-item__text">
-                <ContentText :text="fixText(item.result)" />
+                <ContentText v-if="item.text" :text="fixText(item.text)" />
+                <GlobalRoundVideo class="route-item__video _tablet" v-if="item.video_preview && item.video_link" :preview="item.video_preview" :link="item.video_link"  />
+              </div>
+              <div class="route-item__accordion accordion" v-if="item.list.length">
+                <AccordionItem
+                    v-for="(list, key) in item.list"
+                    :key="key"
+                    :title="list.title"
+                    :counter="( dataKey + 1 ) + '.' + ( key + 1 )"
+                    :text="list.list"
+                    :is-two-column="list.is_two_colums"
+                    title-class="fz-h4"
+                    :is-white="true"
+                    class="_small route-item__accordion-item"
+                />
+              </div>
+              <div class="route-item__result" v-if="item.result">
+                <div class="route-item__subtitle fz-h4 fz-h1--mobile">Результат</div>
+                <div class="route-item__text">
+                  <ContentText :text="fixText(item.result)" />
+                </div>
               </div>
             </div>
           </div>
+          <div class="route__line route__line--last"></div>
         </div>
       </div>
     </div>
@@ -44,6 +67,10 @@
 </template>
 
 <script setup lang="ts">
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import {useHeader} from "~/stores/header";
+
 interface Select {
   post_title?: string,
   video_preview?: string,
@@ -63,6 +90,36 @@ defineProps<{
     select: Array<Select>
   }
 }>();
+
+const items: Ref = ref( null );
+const titles: Ref = ref( null );
+
+const initTitle = ( id: number ) => {
+  const title = titles.value.filter((item: any) => item.dataset.id === id);
+  titles.value.forEach((item: any) => item.classList.remove( 'route__titles-item--active' ));
+  title[0].classList.add( 'route__titles-item--active' );
+}
+
+onMounted( () => {
+  items.value.forEach((item: any) => {
+    ScrollTrigger.create({
+      trigger: item,
+      start: 'top top',
+      end: 'bottom 20%+=100px',
+      onEnter: () => {
+        initTitle( item.dataset.id );
+      },
+
+      onEnterBack: () => {
+        initTitle( item.dataset.id );
+      }
+    });
+  })
+} )
+
+const store = useHeader();
+const isHide = computed(() => store.isHide);
+const headerHeight = computed(() => store.headerHeight);
 </script>
 
 <style scoped lang="scss">
@@ -77,8 +134,10 @@ defineProps<{
   &__grid {
     margin-left: var(--m-m-container);
     margin-right: var(--m-m-container);
-    border-top: 1.5px solid var(--fg-blue);
-    padding: 4rem 2rem 0;
+    //border-top: 1.5px solid var(--fg-blue);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    position: relative;
 
     @media (max-width: $tablet) {
       padding: 0;
@@ -87,16 +146,79 @@ defineProps<{
       margin-right: 0;
     }
   }
+
+  &__col {
+    position: relative;
+    padding: 0 var(--p-container);
+  }
+
+  &__left {
+    padding-top: 4rem;
+    padding-bottom: 4rem;
+  }
+
+  &__right {
+    padding-left: 5.7rem;
+  }
+
+  &__line {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: .15rem;
+    background: var(--fg-blue);
+
+    &--last {
+      top: auto;
+      bottom: 0;
+    }
+
+    &--vertical {
+      width: .15rem;
+      height: 100%;
+      left: 0;
+    }
+  }
+}
+
+.route__titles {
+  padding: 4rem 0;
+  position: sticky;
+  top: 2rem;
+  transition: all .3s;
+  overflow: hidden;
+
+  &-item {
+    display: flex;
+    align-items: flex-start;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin-top: -10rem;
+    transition: margin-top .5s;
+
+    &--active {
+      margin-top: 0;
+    }
+  }
+
+  &-title {
+    margin-left: 1.7rem;
+    max-width: 53rem;
+  }
 }
 
 .route-item {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   padding-bottom: var(--mt-content-third);
   position: relative;
 
+  &:first-child {
+    padding-top: 4rem;
+  }
+
   &:last-child {
-    padding-bottom: 0;
+    padding-bottom: 4rem;
   }
 
   @media (max-width: $tablet) {
@@ -143,11 +265,6 @@ defineProps<{
     align-items: flex-start;
     position: sticky;
     top: 5rem;
-  }
-
-  &__title {
-    margin-left: 1.7rem;
-    max-width: 53rem;
   }
 
   &__subtitle {
