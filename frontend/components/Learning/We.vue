b354b11631df4554f5cfb8a9cc6a5e34c3b61499
Filipp Romanov <59692476+Flk-dev@users.noreply.github.com>
2025-03-06 17:54:58 +0500

    Animate Learing / we

--- frontend/components/Learning/We.vue
+++ frontend/components/Learning/We.vue
@@ -11,20 +11,26 @@
           <div
               class="learning-we__item"
               v-for="(item, key) in data.list"
-              v-intersection-observer="onIntersectionObserver"
+              ref="items"
           >
             <div class="learning-we__item-counter fz-h3">{{ getFormatNumber( (key + 1) ) }}</div>
             <div class="learning-we__item-text fz-h3 fz-h2--mobile" v-html="fixText(item.text)"></div>
           </div>
         </div>
         <div class="learning-we__media">
-          <svg width="520" height="520" viewBox="0 0 520 520" fill="none" xmlns="http://www.w3.org/2000/svg" ref="animatedSvg">
-            <circle cx="260" cy="260" r="259.25" stroke="white" stroke-width="1.5"/>
-            <path d="M260.007 507C281.421 507 298.781 396.414 298.781 260C298.781 123.586 281.421 13 260.007 13C238.592 13 221.232 123.586 221.232 260C221.232 396.414 238.592 507 260.007 507Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
-            <path d="M260 298.774C396.414 298.774 507 281.414 507 259.999C507 238.585 396.414 221.224 260 221.224C123.586 221.224 13 238.585 13 259.999C13 281.414 123.586 298.774 260 298.774Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
-            <path d="M287.429 287.392C383.888 190.933 449.808 100.461 434.666 85.3189C419.524 70.1765 329.052 136.097 232.593 232.556C136.133 329.016 70.2128 419.487 85.3553 434.63C100.498 449.772 190.969 383.852 287.429 287.392Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
-            <path d="M434.666 434.624C449.808 419.482 383.888 329.011 287.429 232.551C190.969 136.092 100.498 70.1712 85.3553 85.3136C70.2128 100.456 136.133 190.927 232.593 287.387C329.052 383.846 419.524 449.767 434.666 434.624Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
-          </svg>
+          <div class="learning-we__round">
+            <svg width="520" height="520" viewBox="0 0 520 520" fill="none" xmlns="http://www.w3.org/2000/svg" ref="circle">
+              <circle cx="260" cy="260" r="259.25" stroke="white" stroke-width="1.5"/>
+            </svg>
+          </div>
+          <div class="learning-we__lines">
+            <svg width="520" height="520" viewBox="0 0 520 520" fill="none" xmlns="http://www.w3.org/2000/svg" ref="lines">
+              <path data-delay="2" data-sync data-duration="80" d="M260.007 507C281.421 507 298.781 396.414 298.781 260C298.781 123.586 281.421 13 260.007 13C238.592 13 221.232 123.586 221.232 260C221.232 396.414 238.592 507 260.007 507Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+              <path data-delay="4" data-sync data-duration="80" d="M260 298.774C396.414 298.774 507 281.414 507 259.999C507 238.585 396.414 221.224 260 221.224C123.586 221.224 13 238.585 13 259.999C13 281.414 123.586 298.774 260 298.774Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+              <path data-delay="6" data-sync data-duration="80" d="M434.666 434.624C449.808 419.482 383.888 329.011 287.429 232.551C190.969 136.092 100.498 70.1712 85.3553 85.3136C70.2128 100.456 136.133 190.927 232.593 287.387C329.052 383.846 419.524 449.767 434.666 434.624Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+              <path data-delay="8" data-sync data-duration="80" d="M287.429 287.392C383.888 190.933 449.808 100.461 434.666 85.3189C419.524 70.1765 329.052 136.097 232.593 232.556C136.133 329.016 70.2128 419.487 85.3553 434.63C100.498 449.772 190.969 383.852 287.429 287.392Z" stroke="white" stroke-width="1.24121" stroke-miterlimit="10"/>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
@@ -34,7 +40,10 @@
 
 <script setup lang="ts">
 import Vivus from "vivus";
-import { vIntersectionObserver } from '@vueuse/components'
+// import { vIntersectionObserver } from '@vueuse/components'
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 defineProps<{
   data: {
@@ -46,26 +55,69 @@ defineProps<{
   }
 }>();
 
-const animatedSvg = ref<Ref|null>(null);
-const vivus = ref( {} );
+const items: Ref = ref( null );
+
+let tl: any;
+
+
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.defaults({
+  markers: false
+});
+
+const circle: Ref = ref(null);
+const lines: Ref = ref(null);
+const vivusCircle: Ref = ref( {} );
+const vivusLines: Ref = ref( {} );
 
 onMounted( () => {
-  vivus.value = new Vivus(
-      animatedSvg.value,
+  tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.learning-we',
+      start: `-20% top`,
+      end: `bottom bottom`,
+      scrub: false,
+      markers: true,
+    }
+  });
+  
+  vivusCircle.value = new Vivus(
+      circle.value,
       {
-        type: 'oneByOne',
-        duration: 800,
+        start: 'manual',
         animTimingFunction: Vivus.EASE,
         pathTimingFunction: Vivus.EASE,
       }
   );
-} );
 
-const onIntersectionObserver = ([entry]: IntersectionObserverEntry[]) => {
-  if (entry?.isIntersecting) {
-    entry.target.classList.add('learning-we__item--active');
-  }
-}
+  vivusLines.value = new Vivus(
+      lines.value,
+      {
+        type: 'oneByOne',
+        duration: 200,
+        delay: .5,
+        start: 'manual',
+        animTimingFunction: Vivus.EASE,
+        pathTimingFunction: Vivus.EASE,
+      }
+  );
+
+  items.value.forEach( (item: any, index: number) => {
+    tl.to( item, {
+      opacity: 1,
+      duration: .5,
+      ease: "Expo.easeOut",
+      onEnter: () => {
+        if ( index === 0 ) {
+          vivusCircle.value.play();
+          setTimeout( () => {
+            vivusLines.value.play();
+          }, 200 )
+        }
+      }
+    } )
+  })
+} )
 </script>
 
 <style scoped lang="scss">
@@ -105,15 +157,9 @@ const onIntersectionObserver = ([entry]: IntersectionObserverEntry[]) => {
   border-top: 1.5px solid var(--fg-white);
   margin-bottom: 4rem;
   opacity: 0;
-  transition: .8s all ease;
-  transition-delay: .8s;
 
   &:last-child {
     margin-bottom: 0;
-  }
-
-  &--active {
-    opacity: 1;
   }
 
   @media (max-width: $tablet) {
@@ -124,9 +170,13 @@ const onIntersectionObserver = ([entry]: IntersectionObserverEntry[]) => {
 }
 
 .learning-we__media {
+  width: 52rem;
+  height: auto;
+  position: relative;
+
   & svg {
-    width: 52rem;
-    height: auto;
+    width: 100%;
+    height: 100%;
   }
 
   @media (max-width: $tablet) {
@@ -134,6 +184,15 @@ const onIntersectionObserver = ([entry]: IntersectionObserverEntry[]) => {
     max-width: 52rem;
     margin: 0 auto 6rem;
   }
+}
+
+.learning-we__lines {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100%;
 }
 
 .learning-we__item-counter {
